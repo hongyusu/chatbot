4d629a340e8804499f06fb1b19f08e4c1154637e
Hongyu Su <hongyu.su@me.com>
2017-04-30 21:39:54 +0300

    minor

--- preprocessing/extractSampleData.py
+++ preprocessing/extractSampleData.py
@@ -3,20 +3,22 @@
 import sys
 import xml.etree.ElementTree
 import os
+import re
+import codecs
 
 def processing(inputFilename, topic, fout):
     root = xml.etree.ElementTree.parse(inputFilename).getroot()
     for c1 in root:
         if c1.get('PostTypeId') == "1":
             try:
-                fout.write("%s, %s\n" % (topic, c1.get('Title')))
+                fout.write('""\t"%s"\t"%s"\n' % ("1", topic, re.sub('"', "", c1.get('Title'))))
             except:
                 print c1.get('Title')
 
 
 if __name__ == '__main__':
     rootdir='/Users/hongyusu/Codes/chatbot/data/stackoverflowdata/'
-    fout = open(rootdir + "sample", "w")
+    fout = codecs.open(rootdir + "sample", "w", encoding="utf-8")
     for root,dirs,filenames in os.walk(rootdir):
         for dir in dirs:
             inputFilename = root+dir+"/Posts.xml"
