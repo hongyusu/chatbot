97c143fce13f640a6df90494eb1a6451d1a76b12
Ubuntu <ubuntu@ip-172-31-19-55.eu-central-1.compute.internal>
2017-05-01 06:09:42 +0000

    readme : initialization : add

---
+++ configuration/init.sh
@@ -0,0 +1,18 @@
+
+
+# pip
+sudo apt-get install python-setuptools python-dev build-essential 
+sudo easy_install pip 
+sudo pip install --upgrade virtualenv 
+
+# packages for intent analysis with deep learning
+sudo pip install keras==1.0.3       
+sudo pip install theano==0.8.2       
+sudo pip install tensorflow==0.12.0rc0       
+sudo pip install pandas==0.19.1       
+sudo pip install scikit-learn=0.18.1       
+sudo pip install flask==0.11.1       
+sudo pip install tweepy==3.5.0       
+sudo pip install h5py==2.6.0
+
+
